5fe15cf2d389d2961d1cb662462f1e9b425cc3e7
Enrique Juan de Dios <juandedioz@gmail.com>
2020-04-10 23:06:05 -0500

    [Setup] Enums and Impl

--- src/main.rs
+++ src/main.rs
@@ -43,14 +43,60 @@ impl Keys {
     }
 }
 
+enum Shape {
+    Rectangle { width: u32, height: u32 },
+    Square(i32),
+    Circle(f64),
+}
+
+impl Shape {
+    fn area(&self) -> f64 {
+        match *self {
+            Shape::Circle(ref r) => 3.14 * r * r,
+            Shape::Square(ref s) => (s * s) as f64,
+            Shape::Rectangle { width, height } => (width * height) as f64,
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Point {
     x: i32,
     y: i32,
 }
 
+fn division(x: f64, y: f64) -> Option<f64> {
+    if y == 0.0 {
+        None
+    } else {
+        Some(x / y)
+    }
+}
+
 fn main() {
+    let c = Shape::Circle(3.5);
+    let s = Shape::Square(34);
+    let r = Shape::Rectangle {
+        width: 76,
+        height: 65,
+    };
+
+    let ac = c.area();
+    println!("Circle Area:{}", ac);
+    let asq = s.area();
+    println!("Square Area:{}", asq);
+    let ar = r.area();
+    println!("Rectangle Area:{}", ar);
     let u = Direction::Up(Point { x: 32, y: 54 });
     let k = u.match_direction();
     println!("Direction Matched:{:?} Key:{}", k, k.destruct());
+
+    let div = division(44.0, 3.0);
+    match div {
+        Some(x) => {
+            println!("Result->{}", x);
+            println!("Result with 2 decimals->{:.2}", x);
+        }
+        None => println!("Can't divide by 0"),
+    }
 }
